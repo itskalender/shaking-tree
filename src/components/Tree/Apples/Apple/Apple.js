5c62cb4ef9ca6d7b33614d02c424c046a3d447d8
toptaskalender <toptaskalender@gmail.com>
2021-04-25 21:51:07 +0300

    Change inline comments

--- src/components/Tree/Apples/Apple/Apple.js
+++ src/components/Tree/Apples/Apple/Apple.js
@@ -8,12 +8,12 @@ import * as actions from '../../../../store/actionCreators';
 import styles from './Apple.module.css';
 
 class Apple extends Component {
-  // I did check this equality to prevent an infinite loop
+  // I did check this equality to ensure that unexpected behaviours are not going to happen.
   shouldComponentUpdate(nextProps, nextState) {
     return this.props.didFall !== nextProps.didFall;
   }
 
-  // onFall event happens whenever one of the apples has 'true' value for didFall property. In actionCreators, I use the 'id' and 'collectingDelay' in an asynchronous way for removing the apples from the ground and putting them into the basket
+  // onFall event happens when one of the apples has been updated and has 'true' value for didFall property. In actionCreators, I use the 'id' and 'collectingDelay' in an asynchronous way for removing the apples from the ground and putting them into the basket
   componentDidUpdate() {
     const collectingDelay =
       this.props.fallingDelay + FALLING_DURATION + WAITING_DURATION;
